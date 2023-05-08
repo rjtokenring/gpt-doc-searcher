66e8079bf6855c39e6fa8e6dcccf1f2c12c6756b
Colombo, Marco <Marco_Colombo@bmc.com>
2023-05-08 17:34:11 +0200

    csv loader example

--- indexer/csv_2_esVector_embeddings.py
+++ indexer/csv_2_esVector_embeddings.py
@@ -1,6 +1,5 @@
-from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.document_loaders import DirectoryLoader
+from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.vectorstores import OpenSearchVectorSearch
 from langchain import OpenAI
 from langchain.chains import RetrievalQA
@@ -9,12 +8,13 @@ import getopt, sys
 argumentList = sys.argv[1:]
  
 # Options
-options = "he:"
+options = "hf:e:"
  
 # Long options
-long_options = ["es_url", "help"]
+long_options = ["file", "es_url", "help"]
 
 es_url = "http://localhost:9200"
+csvfile = "../data-csv/input.csv"
 
 try:
     # Parsing argument
@@ -24,7 +24,10 @@ try:
     for currentArgument, currentValue in arguments:
  
         if currentArgument in ("-h", "--help"):
-            print ("query cli -e <es_url>")
+            print ("data loader -d <dir> -e <es_url>")
+             
+        elif currentArgument in ("-f", "--file"):
+            csvfile = currentValue
              
         elif currentArgument in ("-e", "--es_url"):
             es_url = currentValue
@@ -33,34 +36,24 @@ except getopt.error as err:
     # output error, and return with an error code
     print (str(err))
 
-
-print("Starting query cli...")
+print("Loading data from file {d}".format(d=csvfile))
 print("Connecting to ES cluster {e}".format(e=es_url))
 
 embeddings = OpenAIEmbeddings()
+
+loader = CSVLoader(file_path=csvfile, csv_args={
+    'delimiter': ',',
+    'quotechar': '"'
+})
+documents = loader.load()
 
 docsearch = OpenSearchVectorSearch(index_name="vstore-indexed", 
     embedding_function=embeddings,
     opensearch_url=es_url)
 
-# Query the vector store
-qa = RetrievalQA.from_chain_type(llm=OpenAI(),
-                                chain_type="stuff",
-                                retriever=docsearch.as_retriever())
+print("Starting indexing...")
+print("Discovered {l} documents".format(l=len(documents)))
 
-def query(q):
-    print(f"Query: {q}")
-    print(f"Answer: {qa.run(q)}")
-    print("------------------------------")
+docsearch.add_documents(documents)
 
-
-print("------------------------------")
-
-while True:
- input_variable=input()
- if(input_variable=='quit' or input_variable=='exit'):
-  break
- else:
-  query(input_variable)
-  
-print("Exiting")  
+print("Done...")
